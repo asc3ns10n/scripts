e9f4c68953bcf5262cecc42e16fa51141bfd91bb
asc3ns10n <32691560+asc3ns10n@users.noreply.github.com>
2021-01-19 01:36:58 -0500

    added create_signs_subs

---
+++ create_signs_subs/create_signs_subs.py
@@ -0,0 +1,38 @@
+import argparse
+import glob
+import os
+import sys
+
+import ass
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Create signs files for .ass files in a directory")
+    parser.add_argument('directory', help="""
+    Directory containing .ass files""")
+    parser.add_argument('styles', help="""
+    List of dialogue style names to remove (comma separated)""")
+    args = parser.parse_args()
+
+    styles = args.styles.split(',')
+
+    for file in glob.glob(args.directory+"**/*.ass"):
+        # Get base name
+        base_name = os.path.splitext(file)[0]
+        # Parse sub file
+        with open(os.path.join(args.directory, file), encoding='utf_8_sig') as subs:
+            doc = ass.parse(subs)
+        # Create list of sign events
+        sign_events = []
+        for event in doc.events:
+            if event.style not in styles:
+                sign_events.append(event)
+        doc.events = sign_events
+        # Create signs file
+        signs_file = os.path.join(args.directory, base_name + "_signs.ass")
+        with open(signs_file, 'w', encoding='utf_8_sig') as signs:
+            doc.dump_file(signs)
+        print(f"Created file {signs_file}")
+
+if __name__ == "__main__":
+    sys.exit(main())
